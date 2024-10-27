0adbcdbb7a83ec467c1144c0768329d827b8c3df
mskepasts <66769413+mskepasts@users.noreply.github.com>
2024-10-27 01:46:39 -0400

    latest functioning code

---
+++ scripts/_ss/testEdges.py
@@ -0,0 +1,60 @@
+
+# requirements: pandas
+# requirements: numpy
+# requirements: cv2
+
+
+import numpy as np
+import locale
+locale.setlocale(locale.LC_ALL, 'en_US')
+import pandas as pd
+
+import Rhino.Geometry as rg
+import pyarrow.parquet as pq
+
+def getPointsFromDF(df):
+    x = df['x'].tolist()
+    y = df['y'].tolist()
+    z = df['z'].tolist()
+    print(x)
+    pts = []
+
+    for i in range(len(x)):
+        pt = rg.Point3d(x[i], y[i], z[i])
+        pts.append(pt)
+        i = i+1
+    
+    return pts
+
+
+
+# Read the Parquet file
+table = pq.read_table(file_path)
+
+# Convert to a pandas DataFrame if needed
+df = table.to_pandas()
+df_sample = df.sample(n=globalFilt, random_state=1)
+
+
+dfWalls = df_sample[df_sample['cat'] == 2]
+dfFloor = df_sample[df_sample['cat'] == 1]
+dfCeiling = df_sample[df_sample['cat'] == 0]
+dfWindow =df_sample[df_sample['cat'] == 10]
+
+
+
+df_sampleWalls = dfWalls.sample(n=filt, random_state=1)
+df_sampleFloor = dfFloor.sample(n=filt, random_state=1)
+df_sampleCeiling = dfCeiling.sample(n=filt, random_state=1)
+
+wallPts = getPointsFromDF(df_sampleWalls)
+floorPts = getPointsFromDF(df_sampleFloor)
+ceilingPts = getPointsFromDF(df_sampleCeiling)
+windowPts = getPointsFromDF(dfWindow)
+
+
+
+
+volume = floorPts.delaunay_3d(alpha=2.)
+shell = volume.extract_geometry()
+shell.plot()
--- scripts/test.py
+++ scripts/test.py
@@ -3,12 +3,15 @@ import os
 import pandas as pd
 import numpy as np
 import open3d as o3d
-import trimesh
 import ghhops_server as hs
 import rhino3dm
 from flask import Flask
 import open3d as o3d
-import json
+import cv2
+import outline
+
+
+
 
 def o3d_to_rhino3dm(o3d_mesh):
     """
@@ -28,7 +31,10 @@ def o3d_to_rhino3dm(o3d_mesh):
     # Add vertices to the Rhino mesh
     for vertex in o3d_mesh.vertices:
         rhino_mesh.Vertices.Add(vertex[0], vertex[1], vertex[2])
+        
 
+    for colour in o3d_mesh.vertex_colors:
+        rhino_mesh.VertexColors.Add(int(colour[0]*255),int(colour[1]*255), int(colour[2]*255))
     # Add faces to the Rhino mesh
     for triangle in o3d_mesh.triangles:
         rhino_mesh.Faces.AddFace(triangle[0], triangle[1], triangle[2])
@@ -97,37 +103,104 @@ def dfToPC(df):
     # Convert DataFrame to NumPy array
     points = df[['x', 'y', 'z']].to_numpy()
     # Create an Open3D point cloud object
-    point_cloud = o3d.geometry.PointCloud()
-    point_cloud.points = o3d.utility.Vector3dVector(points)
-    return point_cloud
+    pcd = o3d.geometry.PointCloud()
+
+    # Extract XYZ coordinates
+    points = df[['x', 'y', 'z']].values
+    pcd.points = o3d.utility.Vector3dVector(points)
+
+    # Normalize RGB values to the range [0, 1]
+    colors = df[['r', 'g', 'b']].values / 255.0
+    pcd.colors = o3d.utility.Vector3dVector(colors)
+    return pcd
 
 
 def dfToBB(df):
     return create_bounding_box(dfToPC(df))
 
+def wallsMesh(pline):
+    pts = pline
+    print(pts[0])
+
+def windowMesh(line, zBounds):
+    mesh = rhino3dm.Mesh()
+    ptA = rhino3dm.Point3d(line[0][0], line[0][1], zBounds[0])
+    ptB1 = rhino3dm.Point3d(line[1][0], line[1][1], zBounds[0])
+    ptB = rhino3dm.Point3d(line[0][0], line[0][1], zBounds[1])
+    ptA1 = rhino3dm.Point3d(line[1][0], line[1][1], zBounds[1])
+    
+    mesh.Vertices.Add(ptA.X, ptA.Y, ptA.Z)
+    mesh.Vertices.Add(ptB.X, ptB.Y, ptB.Z)
+    mesh.Vertices.Add(ptA1.X, ptA1.Y, ptA1.Z)
+    mesh.Vertices.Add(ptB1.X, ptB1.Y, ptB1.Z)
+    
+    mesh.Faces.AddFace(0,1,2,3)
+    # Optionally: Compute the normals
+    mesh.Normals.ComputeNormals()
+    
+
+    return mesh
+
+
+
+# def dfToMesh(df):
+    
+#     point_cloud = dfToPC(df)
+
+#     # Downsample the point cloud
+#     pcd = point_cloud.voxel_down_sample(voxel_size=0.01)
+
+#     # Remove outliers
+#     cl, ind = pcd.remove_statistical_outlier(nb_neighbors=20, std_ratio=2.0)
+#     clean_pcd = pcd.select_by_index(ind)
+
+#     # Visualize the cleaned point cloud
+#     # o3d.visualization.draw_geometries([clean_pcd])
+    
+
+#     # Estimate normals
+#     clean_pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
+
+#     # Orient the normals
+#     # clean_pcd.orient_normals_consistent_tangent_plane(100)
+#     mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(
+#         clean_pcd, depth=9
+#     )
+#     mesh.vertex_colors = clean_pcd.colors
+
+#     # # Define a threshold to remove low-density vertices
+#     # vertices = np.asarray(mesh.vertices)
+#     # density_threshold = np.percentile(densities, 5)  # Remove the bottom 5% of densities
+#     # vertices_to_remove = densities < density_threshold
+
+#     # # Filter vertices based on the density threshold
+#     # mesh.remove_vertices_by_mask(vertices_to_remove)
+#     return mesh
 
 def dfToMesh(df):
     
-    point_cloud = dfToPC(df)
+    # Create an open3d PointCloud object
+    pcd = o3d.geometry.PointCloud()
+    points = df[['x', 'y', 'z']].values
+    pcd.points = o3d.utility.Vector3dVector(points)
 
-    # Downsample the point cloud
-    pcd = point_cloud.voxel_down_sample(voxel_size=0.01)
+    
 
-    # Remove outliers
-    cl, ind = pcd.remove_statistical_outlier(nb_neighbors=20, std_ratio=2.0)
-    clean_pcd = pcd.select_by_index(ind)
+    # Normalize RGB values to [0, 1] range and assign them to the point cloud
+    colors = df[['r', 'g', 'b']].values / 255.0
+    pcd.colors = o3d.utility.Vector3dVector(colors)
 
-    # Visualize the cleaned point cloud
-    # o3d.visualization.draw_geometries([clean_pcd])
+    o3d.io.write_point_cloud("colored_pts.ply", pcd)
 
-    # Estimate normals
-    clean_pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.05, max_nn=30))
 
-    # Orient the normals
-    clean_pcd.orient_normals_consistent_tangent_plane(100)
+ 
+    # Estimate normals for Poisson reconstruction
+    pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
+
+    # with o3d.utility.VerbosityContextManager(
+    #         o3d.utility.VerbosityLevel.Debug) as cm:
     mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(
-        clean_pcd, depth=9
-    )
+        pcd, depth=9)
 
     # Define a threshold to remove low-density vertices
     vertices = np.asarray(mesh.vertices)
@@ -136,80 +209,135 @@ def dfToMesh(df):
 
     # Filter vertices based on the density threshold
     mesh.remove_vertices_by_mask(vertices_to_remove)
+    # # Create a mesh using Poisson reconstruction
+    # mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(pcd, depth=9)
+
+    # # Map the colors from the point cloud to the mesh vertices
+    # mesh.vertex_colors = pcd.colors
+
+    # Save the mesh to a .ply file, preserving vertex colors
     return mesh
 
 
+def getPlanarMesh(pLine):
+    rhino3dm.NurbsSurface.Create()
 
+
+def getPolyline(ptList):
+    pts = []
+    if(len(ptList) > 3):
+        ptList = np.append(ptList, [ptList[0]], axis=0)
+        
+    for pt in ptList:
+        pts.append(rhino3dm.Point3d(pt[0],pt[1],0.0))
+    return rhino3dm.Polyline(pts)
 
 def dfToRhinoMesh(df):
-    return o3d_to_rhino3dm(dfToBB(df))
+    return o3d_to_rhino3dm(dfToMesh(df))
 
 def dfToRhinoBBMesh(df):
-    return o3d_to_rhino3dm(dfToMesh(df))
+    return o3d_to_rhino3dm(dfToBB(df))
 # def add_numbers(a: str, b: float) -> rhino3dm.Mesh:
-a = dir_path = os.path.dirname(os.path.realpath(__file__)) + "/setup_22.parquet"
 
-# Read the Parquet file
-table = pq.read_table(a)
 
-# Convert to a pandas DataFrame if needed
-df = table.to_pandas()
-dfWalls = df[df['cat'] == 2]
-dfFloor = df[df['cat'] == 1]
-dfCeiling = df[df['cat'] == 0]
-dfWindow =df[df['cat'] == 10]
+def runAll():
+    from os import listdir
+    from os.path import isfile, join
+    onlyfiles = [f for f in listdir("C:/Users/mskepasts/Documents/GitHub/3d-scan-with-spatial-analysis/data/") if isfile(join("C:/Users/mskepasts/Documents/GitHub/3d-scan-with-spatial-analysis/data/", f))]
 
-if(len(dfWindow) == 0):
-    exit()
+    for file in onlyfiles:
+        fileName = os.path.basename(file).split('/')[-1]
+        run(fileName)
+
+
+def getHeight(walldf):
+    return walldf['Z'].max() - walldf['Z'].min()
+
+
+def run(name):
+    try:
+        a = dir_path = "C:/Users/mskepasts/Documents/GitHub/3d-scan-with-spatial-analysis/" + "/data/" + name
+        table = pq.read_table(a)
+
+    except:
+        exit("File does not exist...")
+
+
+    model = rhino3dm.File3dm()
+    final_fp,room_height,final_endpoint_lst,z_bound = outline.main(a)
+    pLine = getPolyline(final_fp)
+    for i, points in enumerate(final_endpoint_lst):
+        model.Objects.AddMesh(windowMesh(points, z_bound[i]))
+        model.Objects.AddPolyline(getPolyline(points))
     
-print(len(dfWalls))
-print(len(dfFloor))
-print(len(dfCeiling))
+    # Convert to a pandas DataFrame if needed
+    df = table.to_pandas()
+    dfWalls = df[df['cat'] == 2]
+    dfFloor = df[df['cat'] == 1]
+    dfCeiling = df[df['cat'] == 0]
+    dfWindow =df[df['cat'] == 10]
+
+    if(len(dfWindow) == 0):
+        fileName = os.path.basename(a).split('/')[-1]
+        print(str(fileName) + " has no windows!!")
+        return
+        
+    print("Number of points in the walls: " + str(len(dfWalls)))
+    print("Number of points in the floor: " + str(len(dfFloor)))
+    print("Number of points in the ceiling: " + str(len(dfCeiling)))
 
 
 
-df_sampleWalls = dfWalls
-df_sampleFloor = dfFloor
-df_sampleCeiling = dfCeiling
-df_sampleWalls = dfWalls.sample(n=40000, random_state=1)
-df_sampleFloor = dfFloor.sample(n=40000, random_state=1)
-df_sampleCeiling = dfCeiling.sample(n=40000, random_state=1)
+    df_sampleWalls = dfWalls.sample(n=40000, random_state=1)
+    df_sampleFloor = dfFloor
+    df_sampleCeiling = dfCeiling
+    # df_sampleWalls = dfWalls.sample(n=40000, random_state=1)
+    df_sampleFloor = dfFloor.sample(n=40000, random_state=1)
+    df_sampleCeiling = dfCeiling.sample(n=40000, random_state=1)
 
 
-df_sampleWindow = df[df['cat'] == 10]
+    df_sampleWindow = df[df['cat'] == 10]
 
-walls = dfToRhinoMesh(df_sampleWalls)
-floor = dfToRhinoMesh(df_sampleFloor)
-ceiling = dfToRhinoMesh(df_sampleCeiling)
-window = dfToRhinoMesh(df_sampleWindow)
-# Create a 3dm file
-model = rhino3dm.File3dm()
-model.Objects.AddMesh(walls)
-model.Objects.AddMesh(floor)
-model.Objects.AddMesh(ceiling)
-model.Objects.AddMesh(window)
+    walls = dfToRhinoMesh(df_sampleWalls)
+    floor = dfToRhinoMesh(df_sampleFloor)
+    ceiling = dfToRhinoMesh(df_sampleCeiling)
+    window = dfToRhinoMesh(df_sampleWindow)
+    # Create a 3dm file
+    # o3d.io.write_triangle_mesh("colored_mesh.ply", dfToMesh(df_sampleWalls))
 
 
-walls = dfToRhinoBBMesh(df_sampleWalls)
-floor = dfToRhinoBBMesh(df_sampleFloor)
-ceiling = dfToRhinoBBMesh(df_sampleCeiling)
-window = dfToRhinoBBMesh(df_sampleWindow)
+   
+    model.Objects.AddMesh(walls)
+    model.Objects.AddMesh(floor)
+    model.Objects.AddMesh(ceiling)
+    model.Objects.AddMesh(window)
+    model.Objects.AddPolyline(pLine)
+
+    fileName = os.path.basename(a).split('/')[-1]
+    num = fileName[fileName.index("_")+1 : fileName.index(".")]
+
+    # Write the model to a file
+    model.Write("C:/Users/mskepasts/Documents/GitHub/3d-scan-with-spatial-analysis/models/" + "setup_" + str(num)+ ".3dm", 6) 
+
+    print("Rhino Model: " + "setup_" + str(num)+ " saved!")
+    print("\n")
+
+# Defining main function
+def main():
+    
+    num = input("What file number?: ")
+    
+    if num.capitalize() == "All":
+        print("yes")
+        runAll()
+    else:
+        run("setup_" + num + ".parquet")
 
 
-
-
-# Add the point to the model
-model.Objects.AddMesh(walls)
-model.Objects.AddMesh(floor)
-model.Objects.AddMesh(ceiling)
-model.Objects.AddMesh(window)
-
-
-# Write the model to a file
-model.Write("my_geometry.3dm", 6) 
-
-
-
+# Using the special variable 
+# __name__
+if __name__=="__main__":
+    main()
 
 
 
